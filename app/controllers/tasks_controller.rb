c3299a66915dbfd437b975ff278f85c89e0a2645
Ziyad Yehia <ziyadyehia93@gmail.com>
2020-11-10 20:06:45 +0000

    You can now add new tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,9 +4,22 @@ class TasksController < ApplicationController
   end
 
   def new 
+    @task = Task.new
+  end
+
+  def create
+    task = Task.new(task_params)
+    task.save
+    redirect_to tasks_path
   end
 
   def show
     @task = Task.find(params[:id])
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "tasks#index"
-  get "/tasks/new", to: "tasks#new"
+  root to: "tasks#index", as: "tasks"
+  get "/tasks/new", to: "tasks#new", as: "tasks_new"
   get "/tasks/:id/", to: "tasks#show", as: "tasks_show"
+  post "/", to: "tasks#create", as: "tasks_create"
 end
